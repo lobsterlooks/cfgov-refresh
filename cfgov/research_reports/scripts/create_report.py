429a861d9b0b73b211019f9d0c5609127eecd0df
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2020-05-11 14:25:10 -0400

    Send in the .docx file as a script arg

--- cfgov/research_reports/scripts/create_report.py
+++ cfgov/research_reports/scripts/create_report.py
@@ -3,12 +3,11 @@ import subprocess
 from v1.models.base import CFGOVPage
 from v1.models.browse_page import BrowsePage
 
-trial_identifier="programmatic-trial-3"
+trial_identifier="programmatic-trial-4"
 reports_parent_page = CFGOVPage.objects.get(id=1113)
 existing_report = BrowsePage.objects.get(id=13966)   # barf
 
-def get_pandoc_output():
-    report_file_location = "/Users/lortonel/dev/cfgov-project/pandoc/fourth-day-translating-elements/mini-report.docx"
+def get_pandoc_output(report_file_location):
     pandoc_command = ["pandoc", "-f", "docx", "-t", "html", "--standalone", report_file_location]
     result = subprocess.run(pandoc_command, stdout=subprocess.PIPE, universal_newlines=True)
     return result.stdout
@@ -29,10 +28,11 @@ def save_page(page, publish=False):
 
 
 # Invoke this script from the cfgov-refresh root with this command:
-#     cfgov/manage.py runscript create_report
-def run():
+#     cfgov/manage.py runscript create_report --script-args word/doc/file/location.docx
+def run(*args):
+    docx_report_location = args[0]
     print(" *** running pandoc conversion... ***")
-    new_content = get_pandoc_output()
+    new_content = get_pandoc_output(docx_report_location)
     print(" *** making a new page (copying existing report) ***")
     new_report = copy_existing_report()
     print(" *** customizing the new page with pandoc output ***")
